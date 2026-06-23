fb43a87b2dbbf38bb8e367d342755be5b9d0be84
tactidact <16336511+tactidact@users.noreply.github.com>
2026-06-23 13:19:40 +0500

    collatz-conjecture completed

---
+++ collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,12 @@
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    step_count = 0
+    while number != 1:
+        step_count += 1
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = 3 * number + 1
+    return step_count
