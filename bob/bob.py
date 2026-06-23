3d8600d1342cbffd683c8f84df2b0a2ee3138172
tactidact <16336511+tactidact@users.noreply.github.com>
2026-06-23 14:48:24 +0500

    bob completed

---
+++ bob/bob.py
@@ -0,0 +1,11 @@
+def response(hey_bob):
+    hey_bob = hey_bob.strip()
+    if hey_bob == "":
+        return "Fine. Be that way!"
+    if hey_bob[-1] == "?" and hey_bob.isupper():
+        return "Calm down, I know what I'm doing!"
+    if hey_bob[-1] == "?":
+        return "Sure."
+    if hey_bob.isupper():
+        return "Whoa, chill out!"
+    return "Whatever."
